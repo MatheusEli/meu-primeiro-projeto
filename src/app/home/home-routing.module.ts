30fa5aad367c9d304af314b6550f207dfbb613b1
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-09-28 21:55:03 -0300

    Criando branch instagram

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,7 +6,22 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children: [
+
+      {
+        path: 'feed',
+        loadChildren: () => import('../feed/feed.module').then( m => m.FeedPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../profile/profile.module').then( m => m.ProfilePageModule)
+      },
+      {
+        path: 'like',
+        loadChildren: () => import('../like/like.module').then( m => m.LikePageModule)
+      }
+    ]
+  },
 ];
 
 @NgModule({
